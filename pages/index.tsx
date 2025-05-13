c6021c467bcf6e3052b5e17dc32c486aa26956d7
Tân Võ <168068074+tsxcorp@users.noreply.github.com>
2025-05-13 11:31:32 +0700

    Update index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+    return (
+      <div className="p-8 text-center text-gray-600">
+        <h1 className="text-2xl font-semibold">🚀 Welcome to Nexpo Frontend</h1>
+      </div>
+    );
+  }
+  
