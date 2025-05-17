a7d9d67e5a7825eb4aec6e89cabb6c46180f824a
Tân Võ <168068074+tsxcorp@users.noreply.github.com>
2025-05-18 00:48:02 +0700

    chore: ignore .next build folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/_app\":[\"static/media/eafabf029ad39a43-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\",\"static/media/b957ea75a84b6ea7-s.p.woff2\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
