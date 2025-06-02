5ae94cf074af5a1e7c2c55f720b306cb14b9443e
Tân Võ <168068074+tsxcorp@users.noreply.github.com>
2025-06-02 23:25:19 +0700

    update

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:3000';
 
 export const apiClient = axios.create({
   baseURL,
